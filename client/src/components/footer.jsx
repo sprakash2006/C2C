d2168a0740cfe26406f751535eda041924459c50
praka <prakashswami150569@gmail.com>
2025-08-18 16:28:28 +0530

    added footer

---
+++ client/src/components/footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const footer = () => {
+  return (
+    <div className='border-t-2 border-dashed'>
+      <p className='text-white text-2xl bg-[#030f02] py-5 text-center font-mono'>Made By Prakash Swami</p>
+    </div>
+  )
+}
+
+export default footer
