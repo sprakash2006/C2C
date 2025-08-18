5bd10a0afdf7ebaa34ab548936e6e16e43bb2171
praka <prakashswami150569@gmail.com>
2025-08-18 16:33:26 +0530

    added App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,31 @@
 import { useState } from 'react'
 import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import About from '../src/pages/About'
+import Event from '../src/pages/Event'
+import Home from '../src/pages/Home'
+import Join from '../src/pages/Join'
+import Team from '../src/pages/Team'
+import Layout from '../src/Layout/layout'
+import NoPage from'../src/pages/NoPage'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-    <p>careear carftly</p>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="Team" element={<Team />} />
+          <Route path="Events" element={<Event />} />
+          <Route path="About" element={<About />} />
+          <Route path="Join" element={<Join />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
     </>
   )
 }
