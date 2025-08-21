e4965cd73719500791ccad1e49402674e939b476
praka <prakashswami150569@gmail.com>
2025-08-21 15:22:51 +0530

    created admin page

---
+++ src/pages/Admin.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Admin = () => {
+  return (
+    <div className='bg-black text-white'>
+      <h1>This is admin page</h1>
+    </div>
+  )
+}
+
+export default Admin
