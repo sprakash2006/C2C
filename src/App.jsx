5fbb671937f02aafecc5c8f66015687f0114d45f
praka <prakashswami150569@gmail.com>
2025-08-23 17:44:55 +0530

    created private route

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import PrivateRoute from "./PrivateRoute";
 
 import About from '../src/pages/About'
 import Event from '../src/pages/Event'
@@ -10,6 +11,14 @@ import Team from '../src/pages/Team'
 import Layout from '../src/Layout/layout'
 import NoPage from '../src/pages/NoPage'
 import AdminLogin from '../src/pages/AdminLogin'
+import Admin from '../src/pages/Admin'
+import StudentsList from '../src/pages/studentsList'
+import AddUpdate from '../src/pages/addUpdate'
+import DeleteUpdate from '../src/pages/deleteUpdate'
+import EditUpdate from '../src/pages/editupdates'
+import AddEvent from '../src/pages/addEvent'
+import DeleteEvent from '../src/pages/deleteEvent'
+import UpdateEvent from '../src/pages/updateEvent'
 import ScrollToTop from '../src/components/ScrollToTop'
 
 function App() {
@@ -26,6 +35,14 @@ function App() {
           <Route path="About" element={<About />} />
           <Route path="Join" element={<Join />} />
           <Route path="AdLog" element={<AdminLogin />} />
+          <Route path="admin" element={ <PrivateRoute> <Admin /> </PrivateRoute> }/>
+          <Route path="admin/studentslist" element={ <PrivateRoute> <StudentsList /> </PrivateRoute> }/>
+          <Route path="admin/addupdate" element={ <PrivateRoute> <AddUpdate /> </PrivateRoute> }/>
+          <Route path="admin/deleteupdate" element={ <PrivateRoute> <DeleteUpdate /> </PrivateRoute> }/>
+          <Route path="admin/editupdate" element={ <PrivateRoute> <EditUpdate /> </PrivateRoute> }/>
+          <Route path="admin/addevent" element={ <PrivateRoute> <AddEvent /> </PrivateRoute> }/>
+          <Route path="admin/deleteevent" element={ <PrivateRoute> <DeleteEvent /> </PrivateRoute> }/>
+          <Route path="admin/updateevent" element={ <PrivateRoute> <UpdateEvent /> </PrivateRoute> }/>
           <Route path="*" element={<NoPage />} />
         </Route>
       </Routes>
