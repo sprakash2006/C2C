92b19e276cfdceef1cad4fed6db638bd92007add
praka <prakashswami150569@gmail.com>
2025-09-01 23:46:37 +0530

    Updated join us page

--- src/pages/Join.jsx
+++ src/pages/Join.jsx
@@ -10,7 +10,7 @@ const Join = () => {
       <div>
         <Header />
       </div>
-      <div className='pt-50 pb-50'>
+      <div className='pt-50 pb-50' style={{ width: "100%", height: "100%", backgroundColor: "#000000", backgroundAttachment: "fixed", backgroundImage: "linear-gradient(0deg, transparent 24%, rgba(114,114,114,0.2) 25%, rgba(114,114,114,0.2) 26%, transparent 27%, transparent 74%, rgba(114,114,114,0.2) 75%, rgba(114,114,114,0.2) 76%, transparent 77%, transparent), linear-gradient(90deg, transparent 24%, rgba(114,114,114,0.2) 25%, rgba(114,114,114,0.2) 26%, transparent 27%, transparent 74%, rgba(114,114,114,0.2) 75%, rgba(114,114,114,0.2) 76%, transparent 77%, transparent)", backgroundSize: "55px 55px" }}>
         <Form />
       </div>
      </div>
